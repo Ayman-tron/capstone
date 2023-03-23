42eb7294deb8985a06de6924cb638376d84c980c
Ayman-tron <ayman.abdulkadir@ontariotechu.net>
2023-03-22 20:46:00 -0400

    10% highest dominant frequencies

--- read2.py
+++ read2.py
@@ -1,5 +1,3 @@
-# Uses Euclidean norm to get the magnitude of the acceleration in x, y, and z directions
-
 import numpy as np
 import pandas as pd
 from scipy.fft import fft
@@ -7,7 +5,7 @@ from scipy.signal import find_peaks
 import matplotlib.pyplot as plt
 
 # Read the data from the CSV file
-data = pd.read_csv("digital_twin\\2000_pipeline_test.csv")
+data = pd.read_csv("digital_twin\\2000_pipeline_test_loose.csv")
 
 # Extract the acceleration data from the CSV file
 accel_x = data.iloc[:, 1].values
@@ -34,16 +32,25 @@ frequency_bins = np.fft.fftfreq(len(windowed_data), d=1/sampling_rate)
 # Find peaks in the amplitude spectrum
 peak_indices, _ = find_peaks(amplitude_spectrum[:len(amplitude_spectrum)//2])
 
-# Print dominant frequencies and corresponding amplitudes
-print("Dominant frequencies and amplitudes:")
-for i in peak_indices:
+# Get the dominant frequencies and corresponding amplitudes
+dominant_frequencies_amplitudes = [
+    (frequency_bins[i], amplitude_spectrum[i]) for i in peak_indices]
+
+# Sort by amplitude in descending order
+sorted_dominant_frequencies_amplitudes = sorted(
+    dominant_frequencies_amplitudes, key=lambda x: x[1], reverse=True)
+
+# Print the 10% highest dominant frequencies and corresponding amplitudes
+num_top_frequencies = int(len(sorted_dominant_frequencies_amplitudes) * 0.1)
+print("Top 10% dominant frequencies and amplitudes:")
+for i in range(num_top_frequencies):
     print(
-        f"Frequency: {frequency_bins[i]} Hz, Amplitude: {amplitude_spectrum[i]}")
+        f"Frequency: {sorted_dominant_frequencies_amplitudes[i][0]} Hz, Amplitude: {sorted_dominant_frequencies_amplitudes[i][1]}")
 
 # Plot the amplitude spectrum
 plt.plot(frequency_bins[2:len(frequency_bins)//2],
          amplitude_spectrum[2:len(amplitude_spectrum)//2])
 plt.xlabel("Frequency (Hz)")
-plt.ylabel("Amplitude")
+plt.ylabel("Amplitude )")
 plt.title("Amplitude Spectrum")
 plt.show()
